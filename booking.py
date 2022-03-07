5f571eadd423a74eb783af49aa99119e999895b7
saikiranvss <saikiran.kyatham@vishleshan.net>
2022-03-07 20:49:26 +0530

    changes

--- booking.py
+++ booking.py
@@ -3,12 +3,13 @@ import sys
 
 
 class Inventory:
-    # in stock
-    def __init__(self, bikes, cycles, cars, boats):
-        self.bikes = bikes
-        self.cycles = cycles
-        self.cars = cars
-        self.boats = boats
+    bikes = 2
+    cycles = 3
+    cars = 1
+    boats = 2
+    # # in stock
+    def __init__(self):
+        pass
 
     def display_stock(self):
         print(f'We have currently {self.bikes} bikes available to rent.')
@@ -19,26 +20,24 @@ class Inventory:
 
 
 class Customer:
-    def __init__(self):
-        self.customers = []
-        self.customer_name = None
-        self.mobile_number = None
-        self.email_id = None
+    customers = []
 
+    def __init__(self):
+        pass
 
     def view_customer_list(self):
-        if self.customers:
-            for customer in self.customers:
+        if Customer.customers:
+            for customer in Customer.customers:
                 customer_name = customer.get('customer_name')
                 mobile_number = customer.get('mobile_number')
                 email_id = customer.get('email_id')
                 print(f'{customer_name}, {mobile_number}, {email_id}')
 
     def get_customer_names(self):
-        return [customer.get('customer_name') for customer in self.customers]
+        return [customer.get('customer_name') for customer in Customer.customers]
 
     def add_customer(self):
-        self.customers.append({
+        Customer.customers.append({
             'customer_name': input("Please enter your name: "),
             'mobile_number': input("Please enter your mobile number: "),
             'email_id': input("Please enter your email id: ")
@@ -47,13 +46,14 @@ class Customer:
 
 
 class Booking(Customer, Inventory):
-    def __init__(self, rentals):
-        self.rentals = rentals
+    rentals = []
+    def __init__(self):
+        super().__init__()
 
-    def rental_booking(self, customer_obj,inventory_obj):
-        all_customers = customer_obj.get_customer_names()
+    def rental_booking(self):
+        all_customers = self.get_customer_names()
         if all_customers:
-            print('Select the Customer:', *customer_obj.get_customer_names(), sep='\n')
+            print('Select the Customer:', *all_customers, sep='\n')
             customer_name = input("Enter Customer Name: ")
             if customer_name in all_customers:
                 rental_date = input('Enter Rental Date: ')
@@ -61,30 +61,30 @@ class Booking(Customer, Inventory):
                 print('Select the Vehicle Type:', *['bikes', 'cycles', 'cars', 'boats'], sep='\n')
                 vehicle_type = input("Enter Vehicle Type: ")
                 if vehicle_type == 'bikes':
-                    if inventory_obj.bikes <= 0:
-                        print("Sorry! We have currently {} bikes available to rent.".format(self.bikes))
+                    if Inventory.bikes <= 0:
+                        print("Sorry! We have currently {} bikes available to rent.".format(Inventory.bikes))
                         return None
-                    inventory_obj.bikes -= 1
+                    Inventory.bikes -= 1
                 elif vehicle_type == 'cycles':
-                    if inventory_obj.cycles <= 0:
-                        print("Sorry! We have currently {} cycles available to rent.".format(inventory_obj.cycles))
+                    if Inventory.cycles <= 0:
+                        print("Sorry! We have currently {} cycles available to rent.".format(Inventory.cycles))
                         return None
-                    inventory_obj.cycles -= 1
+                    Inventory.cycles -= 1
                 elif vehicle_type == 'cars':
-                    if inventory_obj.cars <= 0:
-                        print("Sorry! We have currently {} cars available to rent.".format(inventory_obj.cars))
+                    if Inventory.cars <= 0:
+                        print("Sorry! We have currently {} cars available to rent.".format(Inventory.cars))
                         return None
-                    inventory_obj.cars -= 1
+                    Inventory.cars -= 1
                 elif vehicle_type == 'boats':
-                    if inventory_obj.boats <= 0:
-                        print("Sorry! We have currently {} boats available to rent.".format(inventory_obj.boats))
+                    if Inventory.boats <= 0:
+                        print("Sorry! We have currently {} boats available to rent.".format(Inventory.boats))
                         return None
-                    inventory_obj.boats -= 1
+                    Inventory.boats -= 1
                 else:
                     print('Invalid Vehicle Type')
                     return None
 
-                self.rentals.append({
+                Booking.rentals.append({
                     'customer_name': customer_name,
                     'rental_date': rental_date,
                     'return_date': return_date,
@@ -100,36 +100,48 @@ class Booking(Customer, Inventory):
 
 
     def show_rentals(self):
-        print(self.rentals)
-
+        if Booking.rentals:
+            print ("{:<8} {:<15} {:<10} {:<10}".format('Name', 'Rental-Date', 'Return-Date', 'Vehicle-Type'))
+            for rental in Booking.rentals:
+                customer_name = rental.get('customer_name')
+                rental_date = rental.get('rental_date', None)
+                return_date = rental.get('return_date', None)
+                vehicle_type = rental.get('vehicle_type', None)
+                print ("{:<8} {:<15} {:<10} {:<10}".format(customer_name, rental_date, return_date, vehicle_type))
+        else:
+            print('No rental bookings are occurred')
 
 def main():
-    inventory_instance = Inventory(2, 3, 1, 2)
+    inventory_instance = Inventory()
     customer_instance = Customer()
-    booking_instance = Booking([])
+    booking_instance = Booking()
     is_done = False
     while not is_done:
-        print(
-            ''' ====== MENU =======
-            1. Add Customer
-            2. Add rental booking
-            3. See Customer List
-            4. See rental booking list
-            5. See Inventory of vehicles available
-            6. Exit
-            '''
-        )
-        choice = int(input("Enter Choice:"))
-        if choice == 1:
-            customer_instance.add_customer()
-        elif choice == 2:
-            booking_instance.rental_booking(customer_instance,inventory_instance)
-        elif choice == 3:
-            customer_instance.view_customer_list()
-        elif choice == 4:
-            inventory_instance.show_rentals()
-        elif choice == 5:
-            inventory_instance.display_stock()
-        elif choice == 6:
-            sys.exit()
+        try:
+            print(
+                ''' ====== MENU =======
+                1. Add Customer
+                2. Add rental booking
+                3. See Customer List
+                4. See rental booking list
+                5. See Inventory of vehicles available
+                6. Exit
+                '''
+            )
+            choice = int(input("Enter Choice:"))
+            if choice == 1:
+                customer_instance.add_customer()
+            elif choice == 2:
+                booking_instance.rental_booking()
+            elif choice == 3:
+                customer_instance.view_customer_list()
+            elif choice == 4:
+                booking_instance.show_rentals()
+            elif choice == 5:
+                inventory_instance.display_stock()
+            elif choice == 6:
+                sys.exit()
+        except Exception as ex:
+            print("Exception Triggered -> ", str(ex))
+            continue
 main()
